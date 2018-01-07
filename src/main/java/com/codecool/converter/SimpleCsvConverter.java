e6c04908cf0f8ee5f863881fbc63255955aa7cc0
Korges <korgesik@gmail.com>
2018-01-07 09:38:42 +0100

    Create SimpleCsvConverter with default outputFormat parameter

---
+++ src/main/java/com/codecool/converter/SimpleCsvConverter.java
@@ -0,0 +1,9 @@
+package com.codecool.converter;
+
+public class SimpleCsvConverter {
+
+    public void converter(String file, String outputFormat) {
+
+        System.out.println("I convert CSV to output format" );
+    }
+}
