0752d9f28d1afd933bb785844c7c1a724e3a7b4f
Korges <korgesik@gmail.com>
2018-01-08 19:09:33 +0100

    Implement XmlOutput

--- src/main/java/com/codecool/converter/Formatter/XmlOutputFormatter.java
+++ src/main/java/com/codecool/converter/Formatter/XmlOutputFormatter.java
@@ -8,6 +8,17 @@ public class XmlOutputFormatter implements OutputFormatter{
 
     public void printToConsole(List<List<String>> data) {
 
-        System.out.println("xml");
+        List<String> label = data.get(0);
+        data.remove(0);
+        System.out.println("<XML>");
+        for (List<String> list : data) {
+
+            System.out.println("\t<ELEMENT>");
+            for(int i =0 ; i<list.size(); i++) {
+                System.out.println("\t\t<" + label.get(i) + ">" + list.get(i) + "</" + label.get(i) + ">");
+            }
+            System.out.println("\t</ELEMENT>");
+        }
+        System.out.println("</XML>");
     }
 }
