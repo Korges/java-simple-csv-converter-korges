af9630464bd1d5d8fa5f54f3831db062be62ba2c
Korges <korgesik@gmail.com>
2018-01-08 19:12:03 +0100

    Rework FileReader class

--- src/main/java/com/codecool/converter/FileReader.java
+++ src/main/java/com/codecool/converter/FileReader.java
@@ -3,23 +3,32 @@ package com.codecool.converter;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
 
     public List<List<String>> readData(String file) {
 
-        String line = "";
         String csvSplitBy = ",";
-        List<List<String>> data = null;
+        List<List<String>> data = new ArrayList<List<String>>();
+
 
         try {
+
             BufferedReader br = new BufferedReader(new java.io.FileReader(file));
-            while ((line = br.readLine()) != null) {
-                List<String> row = null;
-                row.add(line.split(csvSplitBy).toString());
+            String line = br.readLine();
+
+            while (line != null) {
+                List<String> row = new ArrayList<String>();
+                String[] words = line.split(csvSplitBy);
+                for(String s : words) {
+                    row.add(s);
+                }
                 data.add(row);
+                line = br.readLine();
             }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
