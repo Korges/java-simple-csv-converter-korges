7a908f4cb9a5f5455bf039fcc9e49fa41894e760
Korges <korgesik@gmail.com>
2018-01-09 02:14:57 +0100

    Rebuild buffered reader

--- src/main/java/com/codecool/converter/FileReader.java
+++ src/main/java/com/codecool/converter/FileReader.java
@@ -4,29 +4,24 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileReader {
 
     public List<List<String>> readData(String file) {
 
-        String csvSplitBy = ",";
+        String line ="";
         List<List<String>> data = new ArrayList<List<String>>();
-
 
         try {
 
             BufferedReader br = new BufferedReader(new java.io.FileReader(file));
-            String line = br.readLine();
 
-            while (line != null) {
-                List<String> row = new ArrayList<String>();
-                String[] words = line.split(csvSplitBy);
-                for(String s : words) {
-                    row.add(s);
-                }
-                data.add(row);
-                line = br.readLine();
+            while ((line = br.readLine()) != null) {
+
+                data.add(Arrays.asList(line.split(",")));
+
             }
 
         } catch (FileNotFoundException e) {
